53903ec13ec800199987b9fad7eb832f16b0e73a
archymind <31072409+archymind@users.noreply.github.com>
2017-08-16 16:58:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def processRequest(req):
 def makeYqlQuery(req):
     result = req.get("result")
     parameters = result.get("parameters")
-    city = parameters.get("geo-city")
+    city = parameters.get("MartiniqueCity")
     if city is None:
         return None
 
